89572d1d3219e6b9455f84c0e577d0949ea812fa
William Liu <a0937529646@gmail.com>
2019-06-30 22:43:57 +0800

    Add Person Model

---
+++ Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestModel
+{
+    public class Person
+    {
+        private static Person _empty = new Person();
+        public static Person Empty => _empty;
+
+        /// <summary>
+        /// 男:M , 女:F
+        /// </summary>
+        public char gender { get; set; }
+
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+
+        public int Salery { get; set; }
+    }
+}
